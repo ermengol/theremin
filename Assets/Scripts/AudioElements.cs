cff7c0f3d9be61b40f2e2fad546ccbc25eb74235
Antoni Domingo <ermengol87@gmail.com>
2017-07-31 11:40:11 +0200

    1st commit

---
+++ Assets/Scripts/AudioElements.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class AudioElements : MonoBehaviour
+{
+
+	static AudioElements instance;
+
+	public static AudioElements Instance { get { return instance; } }
+
+	Dictionary<int, AudioSource> idTouchToAudioDic = new Dictionary<int, AudioSource> ();
+
+
+	public GameObject FingerAudioPrefab;
+
+	void Awake ()
+	{
+		instance = this;
+	}
+
+	public bool CheckExists (int id)
+	{
+		return idTouchToAudioDic.ContainsKey (id);
+	}
+
+	public void SafeRemoveAudioSource (int id)
+	{
+		if (CheckExists (id))
+		{
+			Destroy (idTouchToAudioDic [id].gameObject);
+			idTouchToAudioDic.Remove (id);
+		}
+	}
+
+	public void CreateAudioSource (int id, float posX, float posY)
+	{
+		if (!CheckExists (id))
+		{
+			GameObject obj = GameObject.Instantiate (FingerAudioPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+			idTouchToAudioDic.Add (id, obj.GetComponent<AudioSource> ());
+		}
+
+		ModifyAudioSource (id, posX, posY);
+	}
+
+	public void ModifyAudioSource (int id, float posX, float posY)
+	{
+		float formalizedX = 3 * (posX / (float)Screen.width);  //Mathf.Clamp(  100 * (posX - 0.53f * Screen.width) / (float)Screen.width, -30,70);
+		float posXToPitch = formalizedX;//Mathf.Pow(1.05946f,formalizedX);
+		float posYToVolume = Mathf.Clamp(posY / (float)Screen.height,0,1);
+
+		idTouchToAudioDic [id].pitch = posXToPitch;
+		idTouchToAudioDic [id].volume = posYToVolume;
+	}
+}
---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class InputInfo
+{
+	public int Id;
+	public float PosX;
+	public float PosY;
+	public TouchPhase Phase;
+
+	public InputInfo (int id, float posX, float posY, TouchPhase phase)
+	{
+		Id = id;
+		PosX = posX;
+		PosY = posY;
+		Phase = phase;
+	}
+}
+
+
+public class TouchManager : MonoBehaviour
+{
+
+
+
+	List<InputInfo> GetInput ()
+	{
+		List<InputInfo> result = new List<InputInfo> ();
+
+		#if UNITY_EDITOR
+		if (Input.GetMouseButton (0))
+		{
+			//Check is inside collider
+
+			if (AudioElements.Instance.CheckExists (0))
+			{
+				result.Add (new InputInfo (0, Input.mousePosition.x, Input.mousePosition.y, TouchPhase.Moved));
+			}
+			else
+			{
+				result.Add (new InputInfo (0, Input.mousePosition.x, Input.mousePosition.y, TouchPhase.Began));
+			}
+		}
+		else if(AudioElements.Instance.CheckExists (0))
+		{
+			result.Add (new InputInfo (0, Input.mousePosition.x, Input.mousePosition.y, TouchPhase.Ended));
+
+		}
+		#endif
+
+		for (int i = 0; i < Input.touches.Length; i++)
+		{
+			Touch input = Input.touches [i];
+			result.Add (new InputInfo (input.fingerId, input.position.x, input.position.y, input.phase));
+		}	
+
+		return result;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//Get all input
+		List<InputInfo> inputs = GetInput ();
+
+		//Manage them
+		ManageInputs (inputs);
+	}
+
+	void ManageInputs (List <InputInfo> inputs)
+	{
+		for (int i = 0; i < inputs.Count; i++)
+		{
+			InputInfo iInfo = inputs [i];
+			if (iInfo.Phase == TouchPhase.Ended)
+			{
+				AudioElements.Instance.SafeRemoveAudioSource (iInfo.Id);
+			}
+			else if (iInfo.Phase == TouchPhase.Began)
+			{
+				AudioElements.Instance.CreateAudioSource (iInfo.Id, iInfo.PosX, iInfo.PosY);
+			}
+			else if (iInfo.Phase == TouchPhase.Moved)
+			{
+				AudioElements.Instance.ModifyAudioSource (iInfo.Id, iInfo.PosX, iInfo.PosY);
+			}
+		}
+	}
+}
